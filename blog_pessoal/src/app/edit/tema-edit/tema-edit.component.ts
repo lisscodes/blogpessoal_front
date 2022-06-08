7e344e3aea2ef8424771047b09d94a858033eee5
lisscodes <lisandra.dias008@gmail.com>
2022-06-07 21:07:53 -0300

    CRUD de tema

---
+++ blog_pessoal/src/app/edit/tema-edit/tema-edit.component.ts
@@ -0,0 +1,46 @@
+import { environment } from 'src/environments/environment.prod';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TemaService } from './../../service/tema.service';
+import { Component, OnInit } from '@angular/core';
+import { Tema } from 'src/app/model/Tema';
+
+@Component({
+  selector: 'app-tema-edit',
+  templateUrl: './tema-edit.component.html',
+  styleUrls: ['./tema-edit.component.css']
+})
+export class TemaEditComponent implements OnInit {
+
+  tema: Tema = new Tema()
+
+  constructor(
+    private temaService: TemaService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    if(environment.token == ''){
+      alert('Sua seção expirou, faça o login novamente.')
+      this.router.navigate(['/entrar'])
+    }
+
+    let id = this.route.snapshot.params['id']
+    this.findByIdTema(id)
+  }
+
+  findByIdTema(id: number){
+    this.temaService.getByIdTema(id).subscribe((resp:Tema) =>{
+      this.tema = resp
+    })
+  }
+
+  atualizar() {
+    this.temaService.putTema(this.tema).subscribe((resp:Tema) =>{
+      this.tema = resp
+      alert('Tema atualizado com sucesso')
+      this.router.navigate(['/tema'])
+    })
+  }
+
+}
